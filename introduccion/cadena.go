f88af50db5ae784677cc5a9b7dc343f1c65028d0
DESKTOP-KSKRR13\Emtycorp <clementemartinez.com@gmail.com>
2016-11-04 00:04:53 -0600

    Subiendo más archivos .go

---
+++ introduccion/cadena.go
@@ -0,0 +1,7 @@
+package main
+import "fmt"
+func main() {
+fmt.Println(len("Hola Mundo"))//longitud de cadena
+fmt.Println("Hola " + "Mundo")//concatenar cadenas
+}
+}
---
+++ introduccion/variables.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main() {
+var x string //primera forma de declarar variables
+x = "Variable uno"
+fmt.Println(x)
+var y string ="Variable dos" //segunda forma de declarar variables
+fmt.Println(y)
+var d ="Max"
+fmt.Println("Mi perro se llama ",d)
+
+}
